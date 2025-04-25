6f0d1fe419f67e0ef8928e934b57e58dff41a7df
FlorianSpreng <spreng.florian@outlook.de>
2025-04-25 19:24:09 +0200

    alle kombinationen werden durchlaufen

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import os
-
 import dotenv
-from black.trans import defaultdict
-from numpy.f2py.auxfuncs import throw_error
 from openai import OpenAI
-from prisma.models import conversation
 
 from processing import get_doctor_system, get_patient_system
 
-dotenv.load_dotenv(".\\.env")
-API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL")
+dotenv.load_dotenv(os.path.join(".", ".env"))
 
 model = "meta-llama-3.1-8b-instruct"  # Choose any available model
 system_content = "You are a helpful assistant"
@@ -27,19 +21,24 @@ user_data = {
 conversation_historie = []
 
 client = OpenAI(
-        api_key=API_KEY,
-        base_url=BASE_URL
-    )
+    api_key=os.getenv("OPENAI_API_KEY"),
+    base_url=os.getenv("BASE_URL")
+)
+
 
 def get_configurations():
     pass
+
 
 def send_message(role, i, system_prompt):
     global message
 
     chat_completion = client.chat.completions.create(
-        messages=[{"role": "system", "content": f"Verhaltensanweisung {system_prompt} + bisheriger Gesprächsverlauf: {conversation_historie}"},
-                  {"role": role, "content": message}
+        messages=[
+            {"role": "system",
+             "content": f"Verhaltensanweisung {system_prompt} + bisheriger Gesprächsverlauf: {conversation_historie}"},
+            {"role": role,
+             "content": message}
                   ],
         model=model,
     )
@@ -51,13 +50,13 @@ def send_message(role, i, system_prompt):
 
 
 def main():
-    for i in range(1, 10):
-        if i % 2 != 0:
-            send_message("arzt", i, get_doctor_system("medic_1"))
-        else:
-            send_message("patient", i, get_patient_system("patient_1"))
-
-
+    for x in range(1,4):
+        for y in range(1,4):
+            for i in range(1, 10):
+                if i % 2 != 0:
+                    send_message("arzt", i, get_doctor_system(f"medic_{x}"))
+                else:
+                    send_message("patient", i, get_patient_system(f"patient_{y}"))
 
 
 if __name__ == "__main__":
