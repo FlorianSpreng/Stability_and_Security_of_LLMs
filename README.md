809373c8331cb210f143b5190e435f18677b21c3
FlorianSpreng <spreng.florian@outlook.de>
2025-04-28 10:09:50 +0200

    backup
    backup for merge

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Stability_and_Security_of_LLMs
+# Stability and Security of LLMs
+This program is used to generate a large number of conversations between a simulated patient and a simulated medic. The project has the following properties:
+
+## Used Models
+This selection of **L**arge **L**anguage **M**odels is based on a list curated by SAIA (**S**calable **A**rtificial **I**ntelligence **A**ccelerator), which is part of the **Gesellschaft für Wissenschaftliche Datenverarbeitung mbH Göttingen**. These free models will be used for the experiment:
+- Llama 3.1 8B Instruct
+- InternVL2.5 8B MPO
+- DeepSeek R1
+- DeepSeek R1 Distill Llama 70B
+- Llama 3.3 70B Instruct
+- Llama 3.1 SauerkrautLM Instruct
+- Llama 3.1 Nemotron 70B
+- Mistral Large Instruct
+- Codestral 22B
+- E5 Mistral 7B Instruct
+- Qwen 2.5 72B Instruct
+- Qwen 2.5 VL 72B Instruct
+- Qwen 2.5 Coder 32B Instruct
+
+## Used Personas
+These are the characters simulated by the models for the conversations.
+
+### Patients
+- Anna Schmidt is experiencing severe shortness of breath, chest pain that worsens with deep breathing, tachycardia, and an unusual cough with possible blood. She is afraid of suffocating and is struggling to breathe and think clearly. Diagnosis: pulmonary embolism.
+- Markus Huber is experiencing sudden, severe eye pain, headaches, blurred vision, halos around lights, and discomfort in the red, dilated eye. The diagnosis is acute glaucoma. The patient is seeking urgent help due to the intense pain and visual disturbances.
+- Laura Fischer has been experiencing a persistent cough for several days, with increasing pain behind the breastbone, shortness of breath, and fever. The diagnosis is acute bronchitis. The patient is anxious and shy about the condition, but now feels concerned due to the prolonged symptoms and hopes it’s not something serious.
+
+### Medics
+- Johannes Krämer is an experienced general practitioner with over 25 years of work in a large hospital. He now practices in a rural setting, where his communication style is patient and thorough, often using relatable analogies to explain medical concepts.
+- Sofia Ben Salem is an experienced internal medicine specialist with a background in multicultural care, having worked for 10 years at a university hospital. She communicates in an analytical, precise manner, and is particularly effective when addressing complex medical cases.
+- Mehmet Yilmaz is a pulmonologist with extensive experience in acute care, having worked in emergency departments for many years. He now runs a practice focused on chronic diseases. His communication style is quick, goal-focused, and slightly ironic, but always caring and direct.
+
+## Conversation Specification
+To guarantee the consistency of conversations, we define several constants to better understand the complexity of the project.
+
+- **Conversation length:** 30 messages
+- **Number of Medics:** 3
+- **Number of Patients:** 3 (fixed)
+- **Number of LLMs:** 12 (as listed above)
+- **Number of Conversations:** <br>
+   $
+   N_{\text{dialogues}} = N_{\text{patient types}} \times N_{\text{medic types}} \times N_{\text{patient LLMs}} \times N_{\text{medic LLMs}} = 3 \times 3 \times 12 \times 12 = 1296 \text{ dialogues}
+   $
+- **Number of Messages:** <br>
+   $
+   N_{\text{messages}} = N_{\text{dialogues}} \times \text{Conversation length} = 1296 \times 30 = 38880 \text{ messages}
+   $
--- main.py
+++ main.py
@@ -1,54 +1,74 @@
+import configparser
 import os
+<<<<<<< Updated upstream
 import dotenv
 from openai import OpenAI
 
+=======
+from datetime import datetime
+
+import dotenv
+from openai import OpenAI
+>>>>>>> Stashed changes
 from processing import get_doctor_system, get_patient_system
 
 dotenv.load_dotenv(os.path.join(".", ".env"))
 
-model = "meta-llama-3.1-8b-instruct"  # Choose any available model
-system_content = "You are a helpful assistant"
-
 message = ""
-
-user_data = {
-    "Vorname": "Florian",
-    "Nachname": "Spreng",
-    "Geschlecht": "Männlich",
-    "Alter": 22
-}
-
 conversation_historie = []
+conversation_log = []
 
 client = OpenAI(
+<<<<<<< Updated upstream
     api_key=os.getenv("OPENAI_API_KEY"),
     base_url=os.getenv("BASE_URL")
 )
 
+=======
+    api_key=API_KEY,
+    base_url=BASE_URL
+)
+>>>>>>> Stashed changes
 
-def get_configurations():
-    pass
+def thoughtchop(message):
+    if "<think>" in message:
+        message = message.split("</think>")[1].replace("\n", "")
+    return message
 
+<<<<<<< Updated upstream
 
 def send_message(role, i, system_prompt):
+=======
+def send_message(role, i, system_prompt, model):
+>>>>>>> Stashed changes
     global message
+    global conversation_historie
+    global conversation_log
 
     chat_completion = client.chat.completions.create(
+<<<<<<< Updated upstream
         messages=[
             {"role": "system",
              "content": f"Verhaltensanweisung {system_prompt} + bisheriger Gesprächsverlauf: {conversation_historie}"},
             {"role": role,
              "content": message}
+=======
+        messages=[{"role": "system", "content": f"Verhaltensanweisung {system_prompt} + bisheriger Gesprächsverlauf: {conversation_historie}"},
+                  {"role": "user", "content": f"{role}: {message}"}
+>>>>>>> Stashed changes
                   ],
-        model=model,
+        model=model
     )
 
-    response = chat_completion.choices[0].message.content
+    conversation_log.append(chat_completion)
+    response = thoughtchop(chat_completion.choices[0].message.content)
+
     print(f"Message={i}, User={role}: {response}")
     conversation_historie.append({"role": role, "content": response})
     message = response
 
 
+<<<<<<< Updated upstream
 def main():
     for x in range(1,4):
         for y in range(1,4):
@@ -57,7 +77,48 @@ def main():
                     send_message("arzt", i, get_doctor_system(f"medic_{x}"))
                 else:
                     send_message("patient", i, get_patient_system(f"patient_{y}"))
+=======
+def safe_conversation_history(model, medic, patient):
+    global conversation_log
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    with open(os.path.join(".log", f"{now}.log"), "w", encoding="utf-8") as f:
+        print(f"Model={model}, medic={medic}, patient={patient}\n")
+        for item in conversation_log:
+            f.write(f"{item}\n")
+    print(f"conversation safed in {now}.log")
 
+def run(medic_role, patient_role, model):
+    print("1")
+    global conversation_historie
+    global conversation_log
+    start_time = datetime.now()
+    print(f"Start", start_time)
+    print("run is running...")
+    for i in range(1, 10):
+        if i % 2 != 0:
+            send_message("arzt", i, get_doctor_system(medic_role), model)
+        else:
+            send_message("patient", i, get_patient_system(patient_role), model)
+    end_time = datetime.now()
+    print("Ende:", end_time)
+
+    delta = end_time - start_time
+    print("Dauer:", delta)
+    print("In Sekunden:", delta.total_seconds())
+    safe_conversation_history(model, medic_role, patient_role)
+    conversation_log = []
+    conversation_historie = []
+>>>>>>> Stashed changes
+
+def main():
+    config = configparser.ConfigParser()
+    with open("config.ini", "r", encoding="utf-8") as f:
+        config.read_file(f)
+    for model in config["models"]:
+        try:
+            run("medic_1", "patient_1", model)
+        except Exception as e:
+            print(f"Fehler bei Modell {model}: {e}")
 
 if __name__ == "__main__":
     main()
